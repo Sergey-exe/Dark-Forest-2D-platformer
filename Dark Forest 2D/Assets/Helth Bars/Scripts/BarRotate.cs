7797430a3ef227068f772d77c344aa00e51dab12
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-06-08 22:02:03 +0300

    Bar update
    Теперь над врагами и игроком появились бары здоровья

---
+++ Dark Forest 2D/Assets/Helth Bars/Scripts/BarRotate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarRotate : MonoBehaviour
+{
+    private Quaternion _startQuaternion;
+
+    private void Start()
+    {
+        _startQuaternion = transform.rotation;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.rotation = _startQuaternion;
+    }
+}
